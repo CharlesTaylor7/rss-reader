f38b9618951151f9a9e3039e2f3b0ad14347219a
Charles Taylor <charlestaylor95@gmail.com>
2024-11-06 14:08:21 -0500

    Use uwsgi cron

--- rss_reader/wsgi.py
+++ rss_reader/wsgi.py
@@ -1,1 +1,11 @@
+import uwsgi
 from rss_reader.app import app as application
+from rss_reader import sync
+
+def sync_feeds(_signum: int):
+    sync.run()
+
+# sync every 24 hours
+SIGNUM = 1
+uwsgi.add_timer(SIGNUM, 60 * 60 * 24)
+uwsgi.register_signal(SIGNUM, "", sync_feeds)
