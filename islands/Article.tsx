e79dfb00792149466927731c2f7b4d8127e25909
Charles Taylor <charlestaylor95@gmail.com>
2026-03-01 20:37:34 -0500

    Deno fmt

--- islands/Article.tsx
+++ islands/Article.tsx
@@ -2,19 +2,21 @@ import type { Signal } from "@preact/signals";
 import { Button } from "@/components/Button.tsx";
 
 function openInNewTab(url) {
-  window.open(url, '_blank').focus();
+  window.open(url, "_blank").focus();
 }
 
 interface ArticleProps {
-  title: string
-  description: string
-  url: string
+  title: string;
+  description: string;
+  url: string;
 }
 
 export default function (props: ArticleProps) {
   return (
-    <div class="card card-side bg-base-100 shadow-sm" onClick={() => openInNewTab("https://www.google.com")}>
-
+    <div
+      class="card card-side bg-base-100 shadow-sm"
+      onClick={() => openInNewTab("https://www.google.com")}
+    >
       <img
         src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
         alt="Movie"
--- routes/index.tsx
+++ routes/index.tsx
@@ -10,7 +10,11 @@ export default define.page(function Home(ctx) {
   return (
     <div class="px-4 py-8 mx-auto fresh-gradient min-h-screen">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center gap-3">
-        <Article title="title 1" description="description 1" url = "www.google.com"/>
+        <Article
+          title="title 1"
+          description="description 1"
+          url="www.google.com"
+        />
         <Article title="title 2" description="description 2" />
       </div>
     </div>
