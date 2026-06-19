1970cf7d677203802c93455703a1db7bb3a0e046
Charles Taylor <charlestaylor95@gmail.com>
2026-06-19 09:32:23 -0400

    Bump version for PWA

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "23";
+const APP_VERSION = "24";
 const ASSETS = ["/manifest.json", "/pwa-icon.webp"];
 
 self.addEventListener("install", (event) => {
@@ -19,7 +19,7 @@ self.addEventListener("activate", (event) => {
             return caches.delete(key);
           }
         }),
-      )
+      ),
     ),
   );
 
