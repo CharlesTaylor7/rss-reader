131c090a4dfdb6472ecceb27f689c5e2fedce8bc
Charles Taylor <charlestaylor95@gmail.com>
2026-07-31 11:21:16 -0400

    Subscribe button provides feedback

--- src/islands/Subscribe.tsx
+++ src/islands/Subscribe.tsx
@@ -1,4 +1,5 @@
 import { SubmitHandler, useForm } from "react-hook-form";
+import { toast } from "@/client/toast.ts";
 
 type Inputs = {
   url: string;
@@ -8,14 +9,19 @@ export function Subscribe() {
   const {
     register,
     handleSubmit,
-    watch,
-    formState: { errors },
   } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (data) =>
-    fetch("/api/subscribe", {
-      method: "POST",
-      body: new URLSearchParams(data),
-    });
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    try {
+      await fetch("/api/subscribe", {
+        method: "POST",
+        body: new URLSearchParams(data),
+      });
+      toast(() => "Subscribed");
+    } catch (e) {
+      const err = e as Error;
+      toast(() => err.message);
+    }
+  };
 
   return (
     <div>
